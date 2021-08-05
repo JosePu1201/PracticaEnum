f6f65eecfc1ef2cfe4ab72f5d1cf84e26093deb9
JosePu <joseluis-pusam@cunoc.edu.gt>
2021-08-05 12:33:09 -0600

    Crear  Repositorio y clases principal

---
+++ src/main/java/com/mycompany/practicaenum/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.practicaenum;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Main {
+    public static  void main(String Args[]){
+        String prueba = "Hola que hace";
+        for (int i = 0; i < prueba.length(); i++) {
+            char a = ' ';
+            a = prueba.charAt(i);
+            System.out.println(a);
+        }
+    }
+}
